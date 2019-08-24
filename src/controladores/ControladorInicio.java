e4836396643c92bc1b5651bb3663f286e0739bb4
Moises <moisespastran_22@hotmail.com>
2019-08-24 14:49:57 -0400

    Proyecto actualizado

--- src/controladores/ControladorInicio.java
+++ src/controladores/ControladorInicio.java
@@ -6,6 +6,19 @@
 package controladores;
 
 import vistas.VPrincipal;
+import modelos.Producto;
+import vistas.VInicio;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import static java.lang.Boolean.TRUE;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import jade.core.Profile; 
+import jade.core.ProfileImpl; 
+import jade.wrapper.*;
+import jade.core.Runtime; 
+import javax.swing.UIManager;
 
 /**
  *
@@ -14,6 +27,7 @@ import vistas.VPrincipal;
 public class ControladorInicio {
     
     private VPrincipal formPrincipal;
+    private VInicio vi;
      
      public ControladorInicio(){
          
@@ -21,6 +35,20 @@ public class ControladorInicio {
        formPrincipal.setVisible(true);
        
        
+     }
+     public void VInicio(){
+    
+    vi = new VInicio();
+    vi.agregar_listeners(this);
+    lm.setVisible(true);
+    lm.setLocationRelativeTo(null);
+    Especialidades();
+    }
+     public void actionPerformed(ActionEvent e) {
+
+        if(((JButton)e.getSource()).getText().equals("Buscar")){
+            
+        }
      }
 
 }
