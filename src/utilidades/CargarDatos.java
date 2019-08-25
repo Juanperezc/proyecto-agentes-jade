03a5f1c7735e3f11ebc97a0c17d506707e4b51bd
Moises <moisespastran_22@hotmail.com>
2019-08-25 09:55:54 -0400

    Imagenes acomodadas

--- src/utilidades/CargarDatos.java
+++ src/utilidades/CargarDatos.java
@@ -14,114 +14,114 @@ public class CargarDatos{
          
          productos.add(new Producto(
          1,
-         "Comodo, acolchado, ligero, cuero, beish",
-         "imagen3.jpg",
-         "Inglese",
+         "Comodo, acolchado, ligero, cuero, beish, talla 15",
+         "zapatos_beige.jpg",
+         "Mercurial 11",
          40.20,
          1));
     
         
          productos.add(new Producto(
          2,
-         "Comodo, flexible, resistente, tela, negro",
-         "imagen2.jpg",
-         "Adidas",
+         "Comodo, flexible, resistente, tela, negro, talla 12",
+         "zapatos_adidas.jpg",
+         "Predator RX",
          50.20,
          1));
          
          productos.add(new Producto(
          3,
-         "Comodo, flexible, ligero, negro, cuero y tela",
-         "imagen10.jpg",
-         "Converse",
+         "Comodo, flexible, ligero, negro, cuero y tela, talla 11",
+         "zapatos_converse.jpg",
+         "All star last generation",
          60.20,
          1));
          
          productos.add(new Producto(
          4,
-         "Brillante, oro 18k",
-         "imagen1.jpg",
-         "Fossil",
+         "Brillante, oro 18k, tamaño radio 4cm",
+         "anillo_oro.jpg",
+         "Sifrina girl",
          20.20,
          3));
          
          productos.add(new Producto(
          5,
-         "Comodas, algodón, negras",
-         "imagen4.jpg",
+         "Comodas, algodón, negras, talla M pequeño",
+         "medias_negras_1.jpg",
          "Night Dress",
          5.20,
          2));
          
          productos.add(new Producto(
          6,
-         "Comodas, algodón, gris con amarillo",
-         "imagen5.jpg",
+         "Comodas, algodón, gris con amarillo, talla L",
+         "medias_grises_1.jpg",
          "Ovejitas 2",
          6.50,
          2));
          
          productos.add(new Producto(
          7,
-         "Ligera, cuero, beish",
-         "imagen6.jpg",
-         "Pull & Bear",
+         "Ligera, cuero, beish, peso 100gr",
+         "cartera1.jpg",
+         "Pavitas G",
          36.50,
          4));
          
          productos.add(new Producto(
          8,
-         "Talla adaptable, Cuero y gamusa, azul y amarillo",
-         "imagen7.jpg",
-         "Ferrari",
+         "Talla adaptable, Cuero y gamusa, azul y amarillo, talla M",
+         "chaqueta.jpg",
+         "Ferrari XSA",
          60.70,
          5));
          
          productos.add(new Producto(
          9,
-         "Absorbentes, comodas, flexibles, negras",
-         "imagen8.jpg",
-         "Roxy",
+         "Absorbentes, comodas, flexibles, negras, talla XX",
+         "medias_mujer.jpg",
+         "Damas de moda",
          24.80,
          2));
          
          productos.add(new Producto(
          10,
-         "Ligera, cierre mágico, 8 bolsillos, azul marino",
-         "imagen9.jpg",
-         "H & M",
+         "Ligera, cierre mágico, 8 bolsillos, azul marino, peso 50 gr",
+         "cartera_A.jpg",
+         "Joven y alocada",
          45.90,
          4));
          
          productos.add(new Producto(
          11,
-         "Comodas, gabaldina, refrescables, negras",
-         "imagen12.jpg",
-         "Embajador",
+         "Comodas, gabaldina, refrescables, vinotinto, talla J",
+         "lenceria2.jpg",
+         "Llamame Bruna",
          3.20,
          2));
          
          productos.add(new Producto(
          12,
-         "Comodas, desechables, bolsillo urinario, negras",
-         "imagen13.jpg",
-         "Rip Curl",
+         "Comodas, desechables, bolsillo urinario, rosadas, talla J",
+         "lenceria3.jpg",
+         "Rippeadora",
          6.20,
          2));
          
          productos.add(new Producto(
          13,
-         "Comodas, algodón, rojas",
-         "imagen14.jpg",
-         "Always",
+         "Comodas, algodón, rojas, talla Z mediana",
+         "lenceria1.jpg",
+         "Always young",
          8.10,
          2));
          
          productos.add(new Producto(
          14,
-         "Estirable, casual para la noche, vinotinto",
-         "imagen16.jpg",
-         "Zara",
+         "Estirable, casual para la noche, vinotinto, talla M grande",
+         "vestido.jpg",
+         "Zara & Yo",
          80.40,
          5));
          
